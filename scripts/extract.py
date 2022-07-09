c11ea53cad9e7bbc481fb27f1c4870530c98fe3b
Max Boyko <maxboykomax@gmail.com>
2022-07-08 23:55:35 -0700

    feat: Download and save the zip

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,8 +1,18 @@
 import argparse
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="Specifies the url for the project files")
 
 args = parser.parse_args()
 
-print(args.url)
+# Get the zip file
+response = requests.get(args.url)
+
+# Print the status code
+print(response.status_code)
+
+# Save the file locally (more about open() in the next lesson)
+local_path = f"tmp/data.zip"
+with open(local_path, "wb") as f:
+    f.write(response.content)
